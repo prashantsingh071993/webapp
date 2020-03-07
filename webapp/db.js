6e8073030e4c81996ad4e9624d56b8b351c80421
Prashant Singh <singh.prasha@husky.neu.edu>
2020-03-06 19:58:13 -0500

    adding config.yml

---
+++ webapp/db.js
@@ -0,0 +1,166 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,
+    {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
+  }
+);
+
+
+
+// const sequelize = new Sequelize("csye6225", "root", "root",
+//   {
+//   host: "localhost",
+//   port: 3306,
+//   dialect: 'mysql'
+// }
+// );
+
+
+class User extends Sequelize.Model {}
+
+User.init(
+  {
+    id: {
+      type: Sequelize.UUID,
+      primaryKey: true
+    },
+    first_name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    last_name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email_address: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+      isEmail: true
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    updatedAt: 'account_updated',
+    createdAt: 'account_created'
+  }
+);
+
+class Bill extends Sequelize.Model {}
+
+Bill.init(
+  {
+    id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+      primaryKey: true
+    },
+    vendor: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    bill_date: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+      categories: {
+          type: Sequelize.JSON
+      },
+
+     due_date: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+      paymentStatus: {
+          type: Sequelize.ENUM({
+              values: ['paid', 'due', 'past_due', 'no_payment_required']
+          }),
+          allowNull: false
+      },
+
+    amount_due: {
+      type: Sequelize.DOUBLE,
+      validate: { min: 0.01, max: 1000000 },
+      allowNull: false
+    },
+      attachment: {
+          type: Sequelize.JSON
+      },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    updatedAt: 'created_ts',
+    createdAt: 'updated_ts'
+  }
+);
+User.hasMany(Bill, { as: 'bills' });
+
+class File extends Sequelize.Model {}
+
+File.init(
+    {
+        id: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            primaryKey: true
+        },
+        file_name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        size: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        md5: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        mime_type: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        upload_date: {
+            type: Sequelize.DATE,
+            allowNull: false
+        },
+        aws_metadata_etag:{
+            type: Sequelize.STRING,
+            allowNull: true
+        },
+        url:{
+            type: Sequelize.STRING,
+            allowNull: true
+        },
+        aws_metadata_key:{
+            type: Sequelize.STRING,
+            allowNull: true
+        },
+        aws_metadata_bucket:{
+            type: Sequelize.STRING,
+            allowNull: true
+        },
+
+    },
+    {
+        sequelize,
+        timestamps: false
+    }
+);
+Bill.hasOne(File);
+
+const init = async () => {
+  await sequelize.authenticate();
+  await sequelize.sync();
+};
+
+module.exports = { User, Bill, File, init };
--- webapp/server.js
+++ webapp/server.js
@@ -1,8 +1,38 @@
-const http = require('http');
-const app = require('./app');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const db = require('./db');
 
-const port = process.env.PORT || 3000;
+const userService = require('./services/user');
+const billService = require('./services/bill');
+const fileService = require('./services/file');
+const fileUpload = require('express-fileupload');
+db.init();
 
-const server = http.createServer(app);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-server.listen(port);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "*");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+app.use(fileUpload());
+
+
+userService(app);
+billService(app);
+fileService(app);
+
+var server = app.listen(process.env.PORT || 3000, function () {
+    var port = server.address().port;
+    console.log("Running on port: ", port);
+});
+
+module.exports = app;
