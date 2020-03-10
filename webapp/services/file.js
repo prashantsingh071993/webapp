17b01f4b8b0f0a785fa7ce4f12ca532e4e32eafb
Prashant Singh <singh.prasha@husky.neu.edu>
2020-03-10 06:06:27 -0400

    changes

--- webapp/services/file.js
+++ webapp/services/file.js
@@ -43,9 +43,9 @@ module.exports = function(app) {
                 throw new Error("Incorrect file type" + ext1);
             }
 
-            await req.files.attachment.mv(
-                `${__dirname}/../uploads/${bid}-${req.files.attachment.name}`
-            );
+            // await req.files.attachment.mv(
+            //     `${__dirname}/../uploads/${bid}-${req.files.attachment.name}`
+            // );
 
             const aws_metadata = await uploadS3.upload('uploads',
                 `${__dirname}/../uploads/${bid}-${req.files.attachment.name}`
@@ -70,10 +70,10 @@ module.exports = function(app) {
             });
 
 
-            await fs.unlink(
-                `${__dirname}/../uploads/${bid}-${req.files.attachment.name}`,() => {
-                    console.log("File deleted");}
-            );
+            // await fs.unlink(
+            //     `${__dirname}/../uploads/${bid}-${req.files.attachment.name}`,() => {
+            //         console.log("File deleted");}
+            // );
 
 
             const fileUpload = {
