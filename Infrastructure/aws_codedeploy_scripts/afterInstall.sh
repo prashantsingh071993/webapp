4010a95d13a2f778d818370f125ead46f33e2192
Prashant Singh <singh.prasha@husky.neu.edu>
2020-03-10 00:26:00 -0400

    changes

--- Infrastructure/aws_codedeploy_scripts/afterInstall.sh
+++ Infrastructure/aws_codedeploy_scripts/afterInstall.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 cd /home/ubuntu
 sudo mv webapp /home/ubuntu/running_webapp
+sudo mv /home/ubuntu/running_webapp /var/
+cd /var
 cd running_webapp
 ls -al
 cd webapp
 sudo npm install
-mkdir vars
-cp /etc/profile.d/custom.sh /home/ubuntu/running_webapp/webapp/vars/
-cd /home/ubuntu/running_webapp/webapp/vars
-chmod 700 custom.sh
-cd ..
-ls -al
+
--- Infrastructure/aws_codedeploy_scripts/applicationStart.sh
+++ Infrastructure/aws_codedeploy_scripts/applicationStart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd /home/ubuntu/running_webapp/webapp
+cd /var/running_webapp/webapp
 pwd
 sudo npm start
