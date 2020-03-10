0c01a8779d4ba07b75fb984630e135b957d5e167
Prashant Singh <singh.prasha@husky.neu.edu>
2020-03-10 00:42:32 -0400

    changes

--- Infrastructure/aws_codedeploy_scripts/afterInstall.sh
+++ Infrastructure/aws_codedeploy_scripts/afterInstall.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 cd /home/ubuntu
-sudo mv webapp /home/ubuntu/running_webapp
-sudo mv /home/ubuntu/running_webapp /var/
-cd /var
-mv /var/webapp /var/running_webapp/
-cd /var
-cd running_webapp
 ls -al
-cd webapp
-sudo npm install
+
 
