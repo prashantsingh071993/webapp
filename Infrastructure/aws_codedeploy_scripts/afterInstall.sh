3b6dbce1d72e22f109bd4905c088d6c796c0e264
Prashant Singh <singh.prasha@husky.neu.edu>
2020-03-10 01:52:01 -0400

    changes

--- Infrastructure/aws_codedeploy_scripts/afterInstall.sh
+++ Infrastructure/aws_codedeploy_scripts/afterInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /home/ubuntu
 sudo mkdir running_webapp
-cp webapp /home/ubuntu/running_webapp/
+cp -r webapp /home/ubuntu/running_webapp/
 cd /home/ubuntu/running_webapp/webapp
 pwd
 sudo npm install --save bcrypt
