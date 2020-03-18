cfaa7c5f6a131eaf7b42875230eebfcd77203f11
Prashant Singh <singh.prasha@husky.neu.edu>
2020-03-17 21:23:22 -0400

    changes

--- Infrastructure/aws_codedeploy_scripts/applicationStart.sh
+++ Infrastructure/aws_codedeploy_scripts/applicationStart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /home/ubuntu/latest_app/webapp
 pwd
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/cloudwatch-agent-config.json -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/cloudwatch-agent-config.json -s
 pm2 start server.js
